d239fcfd23d506afc0668806686fac1f65c7a01f
Sérgio Pugliesi <sergiopugli@gmail.com>
2019-10-15 18:07:27 -0300

    Updates

---
+++ precache-manifest.c48639e6293da122eec1fa6d28b72666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c9911f054be294b67e7da56a907ff9",
+    "url": "/NUEG_pageSortUtility/index.html"
+  },
+  {
+    "revision": "c4d7200a7f59bfce027e",
+    "url": "/NUEG_pageSortUtility/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "21f66486e14063a5ad2c",
+    "url": "/NUEG_pageSortUtility/static/js/2.64a5c4cb.chunk.js"
+  },
+  {
+    "revision": "c4d7200a7f59bfce027e",
+    "url": "/NUEG_pageSortUtility/static/js/main.fe304733.chunk.js"
+  },
+  {
+    "revision": "441e6513ac88bd11a6f2",
+    "url": "/NUEG_pageSortUtility/static/js/runtime-main.feca9854.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/NUEG_pageSortUtility/static/media/logo.25bf045c.svg"
+  }
+]);
